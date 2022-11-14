f548d3ca6e24219ff7beed57d6f1a1868c7e43d5
sharon-welch <sharon.michelle.welch@gmail.com>
2022-11-14 10:33:50 -0500

    notes on promise structure

--- js/main.js
+++ js/main.js
@@ -11,12 +11,21 @@ function updateAllVisualizations(){
 
 //NOTE for team: below in initVis each new instance of a viz is created through calling the specific array that is within promises (array of array). This means that EACH ELEMENT IN THE PROMISES BELOW NEEDS TO STAY IN ORDER
 let promises = [
+
+    //array for circle timeline
+
+    //array for pie chart
     d3.csv("data/scoping_napier.csv", d => {
         //QUESTION - does year need to be a datetime object? -nah
         d.year = +d.year;
         d.value = +d.value;
         return d;
     })
+
+    //array for scatterplot
+
+
+
 ];
 
 Promise.all(promises)
